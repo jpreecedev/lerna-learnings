89ad839e6ddb729a357d1280f4acf5308f89dcd9
Jon Preece <jonpreece@rentalcars.com>
2018-11-15 11:16:20 +0000

    Fiddling

--- packages/app/webpack.babel.js
+++ packages/app/webpack.babel.js
@@ -48,26 +48,7 @@ export default {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: [
-            '@babel/plugin-transform-runtime',
-            [
-              '@babel/plugin-proposal-class-properties',
-              {
-                loose: true
-              }
-            ],
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-transform-async-to-generator',
-            '@babel/plugin-syntax-class-properties',
-            '@babel/plugin-syntax-object-rest-spread',
-            '@babel/plugin-transform-react-inline-elements',
-            '@babel/plugin-transform-react-constant-elements',
-            'react-hot-loader/babel'
-          ]
-        }
+        exclude: /node_modules/
       },
       {
         test: /\.module\.s(a|c)ss$/,
